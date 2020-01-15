e8d60b58922fb7c56045367cad25b3a9ecf9852d
Sujindra Maharjan <sujindra@kodiary.com>
2020-01-15 17:14:30 +0545

    facebook standard events implemented for ios

--- ios/Classes/SwiftFacebookStandardEventsPlugin.swift
+++ ios/Classes/SwiftFacebookStandardEventsPlugin.swift
@@ -1,14 +1,47 @@
 import Flutter
 import UIKit
+import FBSDKCoreKit
 
 public class SwiftFacebookStandardEventsPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "facebook_standard_events", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "com.halloglobal.facebook_standard_events/facebook_standard_events", binaryMessenger: registrar.messenger())
     let instance = SwiftFacebookStandardEventsPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "logEvent":
+        handleLogEvent(call, result)
+        break;
+    case "logPurchaseEvent":
+        handlePurchaseLogEvent(call, result)
+        break;
+    default:
+        result("iOS " + UIDevice.current.systemVersion)
+    }
   }
+    private func handleLogEvent(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let arguments = call.arguments as? [String: Any] ?? [String: Any]()
+        let eventName = arguments["name"] as! String
+        let parameters = arguments["parameters"] as? [String: Any] ?? [String: Any]()
+        if arguments["_valueToSum"] != nil && !(arguments["_valueToSum"] is NSNull) {
+            let valueToDouble = arguments["_valueToSum"] as! Double
+            AppEvents.logEvent(AppEvents.Name(eventName), valueToSum: valueToDouble, parameters: parameters)
+        } else {
+            AppEvents.logEvent(AppEvents.Name(eventName), parameters: parameters)
+        }
+        
+        result(nil)
+    }
+    private func handlePurchaseLogEvent(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let arguments = call.arguments as? [String: Any] ?? [String: Any]()
+        let purchaseAmount = arguments["purchaseAmount"] as! Double
+        let parameters = arguments["parameters"] as? [String: Any] ?? [String: Any]()
+        let currency = arguments["currency"] as! String
+
+        AppEvent.purchased(amount: amount, currency: currency, extraParameters: parameters)
+        
+        result(nil)
+    }
 }
--- ios/facebook_standard_events.podspec
+++ ios/facebook_standard_events.podspec
@@ -10,11 +10,12 @@ A new Flutter plugin for Facebook Standard Events
                        DESC
   s.homepage         = 'http://example.com'
   s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.author           = { 'Hallo Team' => 'email@example.com' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
+  s.dependency 'FBSDKCoreKit', '~> 5.0'
 
   s.ios.deployment_target = '8.0'
 end
